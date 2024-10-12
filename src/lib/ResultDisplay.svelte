d8ec247e27fdeb43c136c7196036b2f62de879da
Florent <florent@nirlab.com>
2024-10-12 19:26:23 +0200

    [ui] Add computation and stuff

--- src/lib/ResultDisplay.svelte
+++ src/lib/ResultDisplay.svelte
@@ -1,12 +1,54 @@
 <script lang="ts">
+    import {subscriptionsFromYearlyExpenses} from "./compute";
+
     export let creditUsed: number;
     export let remainingDays: number;
 
     $: passedDays = 365 - remainingDays
     $: pricePerDay = creditUsed / passedDays
     $: yearlyPrice = pricePerDay * 365
-    $: monthlyPrice = yearlyPrice / 12
+
+    $: subscriptions = subscriptionsFromYearlyExpenses(yearlyPrice)
+    $: best = subscriptions.reduce((lowest, item) => item.yearlyPrice < lowest.yearlyPrice ? item : lowest)
 </script>
 
 <h3>Results</h3>
-<p>Your monthly average expenses: {Math.round(monthlyPrice)} CHF</p>
+<p>Based on your expected yearly credit usage of {yearlyPrice.toFixed(2)} CHF, the best subscriptions is:</p>
+<div class="card">
+    <h4>{best.name} (monthly price: {(best.yearlyPrice / 12).toFixed(2)} CHF)</h4>
+</div>
+<h5>Details:</h5>
+<table>
+    <thead>
+        <tr>
+            <th>Subscription</th>
+            <th>Renewals</th>
+            <th>Yearly Price</th>
+            <th>Monthly Price</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each subscriptions as {name, yearlyPrice, expectedYearlyRenewals}}
+            <tr>
+                <td>{name}</td>
+                <td>{expectedYearlyRenewals.toFixed(2)}</td>
+                <td>{yearlyPrice.toFixed()} CHF</td>
+                <td>{(yearlyPrice / 12).toFixed()} CHF</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
+
+<style>
+    table {
+        margin-left: auto;
+        margin-right: auto;
+        border-collapse: collapse;
+        border: 2px solid white;
+    }
+     th, td {
+        border: 1px solid white;
+        padding: 8px;
+         text-align: center;
+     }
+</style>
