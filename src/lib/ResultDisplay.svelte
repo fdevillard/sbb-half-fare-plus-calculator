ef27ef7d17ebd0ec88535f9d4381e259af5fc9ee
Florent <florent@nirlab.com>
2024-10-12 17:50:29 +0200

    (first)

---
+++ src/lib/ResultDisplay.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let creditUsed: number;
+    export let remainingDays: number;
+
+    $: passedDays = 365 - remainingDays
+    $: pricePerDay = creditUsed / passedDays
+    $: yearlyPrice = pricePerDay * 365
+    $: monthlyPrice = yearlyPrice / 12
+</script>
+
+<h3>Results</h3>
+<p>Your monthly average expenses: {Math.round(monthlyPrice)} CHF</p>
